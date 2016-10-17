d62246f9e04d2aea0f2c5191f2a580a0f02420ba
Kohei Yamamoto <kymmt90@gmail.com>
2016-10-17 16:08:20 +0900

    フォームが空白のとき登録ボタンを無効化

--- app/assets/javascripts/TaskForm.js
+++ app/assets/javascripts/TaskForm.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default class TaskForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activateSubmit: false };
+  }
+
   handleSubmit(e){
     e.preventDefault();
     var content = ReactDOM.findDOMNode(this.refs.content).value.trim();
@@ -13,13 +18,22 @@ export default class TaskForm extends React.Component {
     return;
   }
 
+  handleChange(e) {
+    var content = ReactDOM.findDOMNode(this.refs.content).value.trim();
+    if (!content || /\s+/.test(content)) {
+      this.setState({ activateSubmit: false });
+    } else {
+      this.setState({ activateSubmit: true });
+    }
+  }
+
   render() {
     return (
-      <form className="todoForm" onSubmit={this.handleSubmit.bind(this)}>
+      <form className="todoForm" onSubmit={this.handleSubmit.bind(this)} onChange={this.handleChange.bind(this)}>
         <div className="input-group">
           <input type="text" className="form-control" placeholder="ToDo" ref="content" />
           <span className="input-group-btn">
-            <input type="submit" className="btn btn-primary" value="登録" />
+            <input type="submit" className="btn btn-primary" value="登録" disabled={!this.state.activateSubmit} />
           </span>
         </div>
       </form>
