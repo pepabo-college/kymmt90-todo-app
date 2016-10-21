d9c3548bedb55d2a8641a685aa72e9e700e75488
pyama86 <pyama@pepabo.com>
2016-10-21 16:58:01 +0900

    add capistrano deploy

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+# config valid only for current version of Capistrano
+set :application, 'rails'
+set :repo_url, "https://2b2de14c31e7c048240265c2d608c9f4bf9034a4:x-oauth-basic@github.com/pepabo-college/kymmt90-todo-app.git"
+
+set :user, 'centos'
+set :branch, 'master'
+
+set :default_env, { path: "/home/centos/.rbenv/bin:/home/centos/.rbenv/shims:$PATH" }
+set :bundle_jobs, 8
+
+set :puma_init_active_record, true
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :sidekiq_role, :app
+set :normalize_asset_timestamps, %w{public/images public/javascripts public/stylesheets}
+
+set :rbenv_type, :system
+set :rbenv_ruby, '2.3.1'
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+SSHKit.config.umask = '002'
